8d72c158daec55cb2459d9ac70619efde351d9ff
Bruno <brunorodriguesdias@gmail.com>
2022-10-26 20:36:51 -0300

    adicionando mensagens de erro no cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from usuarios.models import Usuario
+from django.contrib import messages
 
 def cadastro(request):
     if request.method == 'POST':
@@ -10,14 +11,19 @@ def cadastro(request):
         confirma_senha = request.POST['senha-confirme']
         
         if not nome.strip():
+            messages.info(request, 'Nome inválido, tente novamente!')
             return redirect('cadastro')
         if not e_mail.strip():
+            messages.info(request, 'E-mail inválido, tente novamente!')
             return redirect('cadastro')
         if not senha.strip():
+            messages.info(request, 'Senha inválida, tente novamente!')
             return redirect('cadastro')
         if senha != confirma_senha:
+            messages.info(request, 'Senhas divergentes, tente novamente')
             return redirect('cadastro')
         if User.objects.filter(email=e_mail).exists():
+            messages.info(request, 'E-mail já cadastrado!')
             return redirect('cadastro')
 
         usuario = User.objects.create_user(username=nome, email=e_mail, password=senha)
@@ -26,8 +32,10 @@ def cadastro(request):
         telefone = request.POST['celular']
 
         if not c_p_f.strip():
+            messages.info(request, 'CPF inválido, tente novamente!')
             return redirect('cadastro')
         if Usuario.objects.filter(cpf=c_p_f).exists():
+            messages.info(request, 'CPF já cadastrado!')
             return redirect('cadastro')
 
         usuario2 = Usuario(cpf=c_p_f, celular=telefone)
